9a56e8bf227221c5132988e12e893462f8b00c14
eternalbeginner <ini.dwiii@gmail.com>
2022-09-15 06:12:51 +0800

    feat(contact-finalization): finalize contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,6 +1,7 @@
 import { Hero } from "@/components/hero";
 
 import { avatar } from "@/data/hero";
+import { IconMail } from "@tabler/icons";
 
 const ContactPage: React.FC = () => {
   return (
@@ -9,8 +10,13 @@ const ContactPage: React.FC = () => {
         <Hero.Header>
           <Hero.Header.Title>Hi there! I&apos;ve been waiting to work with you.</Hero.Header.Title>
           <Hero.Header.Description>
-            Fill the form below to <strong>send me email</strong>.
+            Let send me an email to further talk about collaboration.
           </Hero.Header.Description>
+          <Hero.Cta>
+            <Hero.Cta.Primary href="mailto:ini.dwiii@gmail.com" icon={IconMail}>
+              Let&apos;s go
+            </Hero.Cta.Primary>
+          </Hero.Cta>
         </Hero.Header>
       </Hero>
     </>
